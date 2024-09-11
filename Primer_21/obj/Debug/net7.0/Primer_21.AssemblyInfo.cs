d4a4728868b31001998b718bfa1cd5e2f007d0c0
Petrova Valentina <valya@chuvsu.ru>
2024-09-11 14:22:44 +0300

    Вторая лабораторная

--- Primer_21/obj/Debug/net7.0/Primer_21.AssemblyInfo.cs
+++ Primer_21/obj/Debug/net7.0/Primer_21.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Primer_21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72dfca9173f8d23e90e43b4c9b4db79728468de9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Primer_21")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Primer_21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
